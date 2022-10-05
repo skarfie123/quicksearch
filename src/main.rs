6ec3ef25ba1167c5616525fa43527f09dc02180e
skarfie123 <rahulpai@rocketmail.com>
2022-10-05 23:14:17 +0100

    handle config command early

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,20 @@ fn main() {
     if args.verbose {
         println!("CLI Args: {:?}", args)
     }
+    // Handle this early as we shouldn't attempt to parse the config if the user just wants the path
+    if let quicksearch::cli::Command::Config = args.command {
+        println!("{}", quicksearch::config::get_config_path());
+        exit(0)
+    }
     let config = match quicksearch::config::QuicksearchConfig::parse(&args) {
         Ok(config) => config,
         Err(e) => {
             match e {
                 config::ConfigError::FileParse { uri: _, cause } => {
-                    eprintln!("Error parsing config file: {cause}")
+                    eprintln!("Error while parsing config: {cause}")
                 }
-
-                config::ConfigError::Message(msg) => eprintln!("Error: {msg}"), // at least Missing fields
-                e => eprintln!("Unexpected error: {e}"),
+                config::ConfigError::Message(msg) => eprintln!("Error while parsing config: {msg}"), // at least Missing fields
+                e => eprintln!("Unexpected error while parsing config: {e}"),
             }
             exit(1)
         }
@@ -25,6 +29,7 @@ fn main() {
     match args.command {
         quicksearch::cli::Command::List => list(config),
         quicksearch::cli::Command::Search(_) => search(config, args),
-        quicksearch::cli::Command::Config => println!("{}", quicksearch::config::get_config_path()),
+        // Config command is handled earlier
+        quicksearch::cli::Command::Config => panic!("Unexpected command"),
     }
 }
